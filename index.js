1e9238d8c5a9b16b72a9f8c9826d42dc14adc2a1
Laurel Herting <laurel.herting@gmail.com>
2018-02-02 12:38:03 -0700

    fix spelling error

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const Hapi = require('hapi');
 const server = new Hapi.Server({ port: 3000, host: 'localhost' });
 
 server.start();
-console.log('Server running at: ${server.info.url}');
+console.log('Server running at: ${server.info.uri}');
